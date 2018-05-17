a1c948398ca7a030bac070e91405f00a0e11465f
ThisIsNSH <hadanis.singh@gmail.com>
2018-05-17 23:58:27 +0530

    async request

--- app/src/main/java/com/nsh/networking/MainActivity.java
+++ app/src/main/java/com/nsh/networking/MainActivity.java
@@ -3,6 +3,7 @@ package com.nsh.networking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import com.ibm.watson.developer_cloud.assistant.v1.Assistant;
 import com.ibm.watson.developer_cloud.assistant.v1.model.InputData;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
+import com.ibm.watson.developer_cloud.assistant.v1.model.RuntimeIntent;
 import com.ibm.watson.developer_cloud.http.ServiceCallback;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hit = findViewById(R.id.hit);
+        input_field = findViewById(R.id.input);
+        output_field = findViewById(R.id.output);
 
         assistant = new Assistant(
                 "2018-02-16",
@@ -36,9 +41,29 @@ public class MainActivity extends AppCompatActivity {
 
         assistant.setEndPoint("https://gateway.watsonplatform.net/assistant/api");
 
-        hit = findViewById(R.id.hit);
-        input_field = findViewById(R.id.input);
-        output_field = findViewById(R.id.output);
+        RuntimeIntent intent = new RuntimeIntent();
+        intent.setIntent("welcome");
+        MessageOptions options = new MessageOptions.Builder(workspaceId)
+                .addIntent(intent)
+                .build();
+        assistant.message(options).enqueue(new ServiceCallback<MessageResponse>() {
+            @Override
+            public void onResponse(MessageResponse response) {
+                output_field.setText(response.getOutput().getText().get(0));
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+
+        input_field.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input_field.setInputType(InputType.TYPE_CLASS_TEXT);
+            }
+        });
 
         hit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 assistant.message(options).enqueue(new ServiceCallback<MessageResponse>() {
                     @Override
                     public void onResponse(MessageResponse response) {
-                        output_field.setText(response.getOutput().getText().get(0).toString());
+                        output_field.setText(response.getOutput().getText().get(0));
                     }
 
                     @Override
